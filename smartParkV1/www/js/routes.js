2d60d87232b12197b92d0faf49098e559bf401b7
davidAvigad <david.avigad@gmail.com>
2017-03-26 13:37:21 +0300

    newRepo

---
+++ smartParkV1/www/js/routes.js
@@ -0,0 +1,67 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.sMARTPARK', {
+    url: '/index',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/sMARTPARK.html',
+        controller: 'sMARTPARKCtrl'
+      }
+    }
+  })
+
+  .state('sMARTPARKIN', {
+    url: '/in',
+    templateUrl: 'templates/sMARTPARKIN.html',
+    controller: 'sMARTPARKINCtrl'
+  })
+
+  .state('menu.sMARTPARKOUT', {
+    url: '/out',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/sMARTPARKOUT.html',
+        controller: 'sMARTPARKOUTCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('sMARTPARKLogin', {
+    url: '/login',
+    templateUrl: 'templates/sMARTPARKLogin.html',
+    controller: 'sMARTPARKLoginCtrl'
+  })
+
+  .state('sMARTPARKSignup', {
+    url: '/signup',
+    templateUrl: 'templates/sMARTPARKSignup.html',
+    controller: 'sMARTPARKSignupCtrl'
+  })
+
+  .state('chooseSMARTPARK', {
+    url: '/in-choose',
+    templateUrl: 'templates/chooseSMARTPARK.html',
+    controller: 'chooseSMARTPARKCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
