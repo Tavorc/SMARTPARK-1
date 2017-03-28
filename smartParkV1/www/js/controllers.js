14d41a71eeda3c55311f038d5b9b37c68f255187
davidAvigad <david.avigad@gmail.com>
2017-03-28 11:17:56 +0300

    28MAR/11:17

--- smartParkV1/www/js/controllers.js
+++ smartParkV1/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', ['ionic.cloud'])
+angular.module('app.controllers', [])//'ionic.cloud'
 
 .controller('sMARTInCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
