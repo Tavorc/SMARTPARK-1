7b4606d001b847ec8c515202e58ce99b03800096
Tavorc <tavorcoh@gmail.com>
2017-03-31 17:21:29 +0300

    login +logout work,current location work,client side of notification work
    login +logout work,current location work,client side of notification
    work

--- smartParkV1/www/js/controllers.js
+++ smartParkV1/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])//'ionic.cloud'
+angular.module('app.controllers', ['ionic.cloud' , 'ngCordova'])//'ionic.cloud'
 
 .controller('sMARTInCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -16,19 +16,42 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('sMARTPARKCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('sMARTPARKCtrl', ['$scope', '$stateParams', '$ionicPush',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicPush, $ionicPopup) {
+$scope.$on('cloud:push:notification', function(event, data) {
+  var msg = data.message;
+  	alert(msg.title + ': ' + msg.text);
+	});
+	}])
 
-
-}])
-
-.controller('sMARTMenuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('sMARTMenuCtrl', ['$scope', '$stateParams', '$ionicLoading', '$ionicActionSheet', '$state',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($scope, $stateParams, UserService, $ionicActionSheet, $state) {
+	$scope.googleLogOut = function() {
+		var hideSheet = $ionicActionSheet.show({
+			destructiveText: 'Logout',
+			titleText: 'Are you sure you want to logout?',
+			cancelText: 'Cancel',
+			cancel: function() {},
+			buttonClicked: function(index) {
+				return true;
+			},
+			destructiveButtonClicked: function(){
+				window.plugins.googleplus.logout(
+					function (msg) {
+						console.log(msg);
+						$state.go('sMARTPARKLogin');
+					},
+					function(fail){
+						console.log(fail);
+					}
+				);
+			}
+		});
+	};
 
 }])
 
@@ -47,9 +70,10 @@ function ($scope, $stateParams) {
 
     // uiGmapGoogleMapApi is a promise.
     // The "then" callback function provides the google.maps object.
-    uiGmapGoogleMapApi.then(function(maps){
+    uiGmapGoogleMapApi.then(function(maps, $cordovaGeolocation){
         // Configuration needed to display the road-map with traffic
         // Displaying Ile-de-france (Paris neighbourhood)
+
         $scope.map = {
             center: {
               latitude: -23.598763,
@@ -67,6 +91,7 @@ function ($scope, $stateParams) {
             showTraficLayer:true
         };
     });
+   
 }])
 
 // .controller('sMARTPARKLoginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
@@ -77,19 +102,17 @@ function ($scope, $stateParams) {
 //
 // }])
 
-.controller('sMARTPARKLoginCtrl', ['$scope', '$state','$ionicLoading', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('sMARTPARKLoginCtrl', ['$scope', '$state','$ionicLoading', '$ionicPush', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $state, $ionicLoading) {
+function ($scope, $state, $ionicLoading, $ionicPush) {
 $scope.googleSignIn = function() {
     $ionicLoading.show({
       template: 'Logging in...'
     });
-
     window.plugins.googleplus.login(
       {},
       function (user_data) {
-        // For the purpose of this example I will store user data on local storage
         console.log(user_data);
         // UserService.setUser({
         //   userID: user_data.userId,
@@ -100,13 +123,19 @@ $scope.googleSignIn = function() {
         //   idToken: user_data.idToken
         // });
         $ionicLoading.hide();
-         $state.go('menu.sMARTPARK');
+         $state.go('tabsController.sMARTPARK_tab2');
       },
       function (msg) {
       	 $ionicLoading.hide();
       }
     );
   };
+
+  $ionicPush.register().then(function(t) {
+  		return $ionicPush.saveToken(t);
+			}).then(function(t) {
+ 				 console.log('Token saved:', t.token);
+			});
 
 }])
 
--- smartParkV1/www/js/googlemaps.init.js
+++ smartParkV1/www/js/googlemaps.init.js
@@ -14,61 +14,92 @@ angular.module('googlemaps.init', ['uiGmapgoogle-maps'])
     */
 
     function(uiGmapGoogleMapApi, $timeout) {
-
         return {
             restrict: 'E',
             scope: true,
             link: function($scope, $element, $attr, _c, transclude) {
-            
                 $scope.map = {};
-                
                 if ($attr.marker=="true"){
                     $scope.map.marker = {
                         id: 0
                     }
+                    $scope.map.marker.options = {
+                        icon:'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+                    }
                 }
-                
                 $attr.$observe('location', function(val){
-                    
                     uiGmapGoogleMapApi.then(function(maps){
-
-                        function setupMap(lat, lng){
-                            
-                            $scope.map.zoom = parseInt($attr.zoom);
-                            $timeout(function(){
-                                $scope.map.center = {
-                                    latitude: lat,
-                                    longitude: lng
-                                };  
+                        var Latitude = undefined;
+                        var Longitude = undefined;
+                        $scope.map.zoom = parseInt(14);
+                        function getMapLocation()
+                        {
+                        navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
+                        }
+                         function geolocationSuccess(position)
+                             {
+                              var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                              var geocoder = new google.maps.Geocoder();  
+                              Latitude=position.coords.latitude;
+                              Longitude=position.coords.longitude;
+                              setupMap(Latitude, Longitude);
+                              }
+                        function geolocationError(error)
+                         {
+                           $ionicPopup.alert({
+                           title: "Error Location",
+                           subTitle: "Error",
+                           template: JSON.stringify(error)
                             });
-                            $scope.map.options = JSON.parse($attr.options);
-                            
+                        }
+                        function setupMap(lat, lng){
+                             $timeout(function(){
+                                $scope.map.center = {
+                                    latitude: Latitude,
+                                    longitude: Longitude
+                                };  
+                             });
+                              $scope.map.options = JSON.parse($attr.options);
                             if ($attr.marker=="true"){
                                 $scope.map.marker.coords = {
-                                    latitude: lat,
-                                    longitude: lng
+                                latitude: Latitude,
+                                longitude: Longitude
                                 }
-                            }
+                            }  
+                            console.log($scope.map.marker.coords.latitude);
                         }
-
+                         var  onSuccess = function(position)
+                            {
+                            var lati  = position.coords.latitude;
+                            var long = position.coords.longitude;
+                            if (lati != Latitude && long != Longitude)
+                             {
+                                Latitude = lati;
+                                Longitude = long;
+                                setupMap(lati, long);
+                             }
+                             console.log(lati + ":" + long);
+                            }
+                            function onError(error)
+                             {
+                             alert('code: '    + error.code    + '\n' +
+                                'message: ' + error.message + '\n');
+                             }
+                        var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 3000, enableHighAccuracy: true });
                         if (val.indexOf('"latitude"') > -1){
                             val = JSON.parse(val);
                             setupMap(val.latitude, val.longitude);
                         }else{
                             var geocoder = new maps.Geocoder();
                             geocoder.geocode({'address' : val}, function(results, status){
-                                
                                 $scope.$apply(function(){
                                     setupMap(results[0].geometry.location.lat(), results[0].geometry.location.lng());
                                 });
             
                             });
                         }
-                    });
-                            
+                    });       
                 });
-            
             }
-      
         };
 }]);
