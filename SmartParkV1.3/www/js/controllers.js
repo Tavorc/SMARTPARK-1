3c40fb36f8bda698ae735be3860639c1f325af7d
Tavorc <tavorcoh@gmail.com>
2017-05-10 14:25:22 +0300

    fix sign up
    fix sign up

--- SmartParkV1.3/www/js/controllers.js
+++ SmartParkV1.3/www/js/controllers.js
@@ -649,10 +649,10 @@ function ($scope, $stateParams) {
 }
 })
 
-.controller('signupCtrl', ['$scope', '$state', '$stateParams', '$ionicAuth', '$ionicUser', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('signupCtrl', ['$scope', '$state', '$stateParams', '$ionicAuth', '$ionicUser','UserService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $state, $stateParams, $ionicAuth, $ionicUser) {
+function ($scope, $state, $stateParams, $ionicAuth, $ionicUser, UserService) {
     console.log($stateParams);
     $scope.formSignupParams = {
     name : $stateParams.name,
