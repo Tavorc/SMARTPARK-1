ce091ee5350aeb03171621fa6567c2ee050b91c6
Tavorc <tavorcoh@gmail.com>
2017-05-21 20:23:16 +0300

    show the name in profile

--- SmartParkV1.3/www/js/controllers.js
+++ SmartParkV1.3/www/js/controllers.js
@@ -779,11 +779,13 @@ getLocation (function(locationResult){
 }])
 
 
-.controller('myProfileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('myProfileCtrl', ['$scope', '$stateParams', 'UserService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($scope, $stateParams, UserService) {
+var userN=UserService.getUser().givenName;
+console.log("user: " + userN);
+  $scope.userName=userN;
 
 }])
 
